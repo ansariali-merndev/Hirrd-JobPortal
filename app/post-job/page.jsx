5478b5978323c41feaef39b2aa8db3449b63799c
ansariali <=>
2025-05-18 22:06:03 +0530

    Post-job ready

--- app/post-job/page.jsx
+++ app/post-job/page.jsx
@@ -1,7 +1,126 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+
 export default function PostJob() {
+  const initialValue = {
+    title: "",
+    description: "",
+    city: "",
+    company: "",
+    skills: "",
+  };
+  const [inputValue, setInputValue] = useState(initialValue);
+
+  const handleInputValue = (e) => {
+    const { name, value } = e.target;
+    setInputValue((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(initialValue);
+    setInputValue(initialValue);
+  };
+
   return (
-    <div>
-      <h1>PostJob</h1>
-    </div>
+    <section>
+      <div className="my-8">
+        <h1 className="text-2xl sm:text-4xl lg:text-6xl font-extrabold bg-gradient-to-br from-purple-600 via-orange-600 text-transparent bg-clip-text text-center my-6">
+          Post a Job
+        </h1>
+        <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
+          <Input
+            type={"text"}
+            placeholder="Job Title"
+            value={inputValue.title}
+            onChange={handleInputValue}
+            className={"w-full"}
+            name="title"
+          />
+          <Textarea
+            placeholder="Type your Job Description here."
+            className={"w-full"}
+            value={inputValue.description}
+            onChange={handleInputValue}
+            name="description"
+          />
+          <section className="grid grid-cols-2 w-full gap-2">
+            {/* First Section */}
+            <Select
+              onValueChange={(value) =>
+                setInputValue((prev) => ({ ...prev, city: value }))
+              }
+            >
+              <SelectTrigger className={"w-full"}>
+                <SelectValue placeholder="Select City Name" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="Mumbai">Mumbai</SelectItem>
+                  <SelectItem value="New Delhi">New Delhi</SelectItem>
+                  <SelectItem value="Noida UP">Noida UP</SelectItem>
+                  <SelectItem value="Bangalore">Bangalore</SelectItem>
+                  <SelectItem value="Hyderabad">Hyderabad</SelectItem>
+                  <SelectItem value="Pune">Pune</SelectItem>
+                  <SelectItem value="Other">Other</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            {/* Second Section */}
+            <Select
+              className="w-full"
+              onValueChange={(value) =>
+                setInputValue((prev) => ({ ...prev, company: value }))
+              }
+            >
+              <SelectTrigger className={"w-full"}>
+                <SelectValue placeholder="Company Name" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="Adobe">Adobe</SelectItem>
+                  <SelectItem value="Amazon">Amazon</SelectItem>
+                  <SelectItem value="Atlassian">Atlassian</SelectItem>
+                  <SelectItem value="Deloitte">Deloitte</SelectItem>
+                  <SelectItem value="Google">Google</SelectItem>
+                  <SelectItem value="Jira">Jira</SelectItem>
+                  <SelectItem value="Tcs">Tcs</SelectItem>
+                  <SelectItem value="Other">Other</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </section>
+          <Textarea
+            placeholder="Skills Required and Responsibility"
+            rows={6}
+            className={"h-50"}
+            value={inputValue.skills}
+            onChange={handleInputValue}
+            name="skills"
+          />
+          <Button
+            type="submit"
+            variant={"secondary"}
+            className={"cursor-pointer"}
+          >
+            Post Job
+          </Button>
+        </form>
+      </div>
+    </section>
   );
 }
